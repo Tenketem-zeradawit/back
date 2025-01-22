4ada508fa75656e70611ceecc3c70d33e7ff8a74
Tenketem-zeradawit <titihadas123@gmail.com>
2025-01-22 18:06:42 +0100

    Update config.js

--- config.js
+++ config.js
@@ -3,7 +3,7 @@ const mongoose = require("mongoose");
 const connectDatabase = async () => {
   try {
     const connection = await mongoose.connect(
-    "mongodb+srv://tenketem-zeradawit:OYlXjPI6xJcfepdG@cluster0.xzyzn.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
+    "mongodb+srv://tenketem-zeradawit:nLlw9S5w4iGdqGe8@cluster0.xzyzn.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
     );
 
     console.log("Database Connected");
